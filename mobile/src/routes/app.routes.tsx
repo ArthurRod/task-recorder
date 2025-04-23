f109b89f01d5b7dcd9558b1233684311c7ffbe07
Arthur Rodrigues <arthur9713@hotmail.com>
2025-04-22 21:17:13 -0300

    feat: add active bottom tab treatment

--- mobile/src/routes/app.routes.tsx
+++ mobile/src/routes/app.routes.tsx
@@ -1,14 +1,38 @@
+import {MaterialIcons} from "@expo/vector-icons";
 import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
 import Home from "../screens/Home";
 import Tasks from "../screens/Tasks";
 
 const Tab = createBottomTabNavigator();
 
+const routes = [
+  {name: "Home", component: Home, icon: "home"},
+  {name: "Tasks", component: Tasks, icon: "list"},
+];
+
 export function AppRoutes() {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Home" component={Home} />
-      <Tab.Screen name="Tasks" component={Tasks} />
+    <Tab.Navigator
+      screenOptions={({route}) => ({
+        tabBarIcon: ({color, size}) => {
+          const currentRoute = routes.find((r) => r.name === route.name);
+          const iconName = currentRoute?.icon ?? "help";
+
+          return (
+            <MaterialIcons name={iconName as any} size={size} color={color} />
+          );
+        },
+        tabBarActiveTintColor: "green",
+        tabBarInactiveTintColor: "black",
+      })}
+    >
+      {routes.map((route) => (
+        <Tab.Screen
+          key={route.name}
+          name={route.name}
+          component={route.component}
+        />
+      ))}
     </Tab.Navigator>
   );
 }
