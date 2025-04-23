c021cdd1347d7000450f1e430d78f1b6a9d5ae08
Arthur Rodrigues <arthur9713@hotmail.com>
2025-04-22 21:08:44 -0300

    feat: create api routes with `gorilla/mux`

---
+++ server/routes/task_routes.go
@@ -0,0 +1,20 @@
+package routes
+
+import (
+	"net/http"
+
+	"task-recorder-server/controllers"
+
+	"github.com/gorilla/mux"
+)
+
+func SetupRoutes() *mux.Router {
+	r := mux.NewRouter()
+
+	r.HandleFunc("/tasks", controllers.GetTasks).Methods(http.MethodGet)
+	r.HandleFunc("/tasks", controllers.CreateTask).Methods(http.MethodPost)
+	r.HandleFunc("/tasks/{id}", controllers.UpdateTaskByID).Methods(http.MethodPut)
+	r.HandleFunc("/tasks/{id}", controllers.DeleteTaskByID).Methods(http.MethodDelete)
+
+	return r
+}
